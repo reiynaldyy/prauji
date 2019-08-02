100d43a5e20800760d3be63fc76e256cb66d42e3
reiynaldyy <reiynaldy321@gmail.com>
2019-08-02 09:35:58 +0700

    asd

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'reiynaldy'),
             'username' => env('DB_USERNAME', 'reypurdy'),
-            'password' => env('DB_PASSWORD', 'aldinama321'),
+            'password' => env('DB_PASSWORD', 'aldinama123'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
